74ebbd484576e33d03e683bc1e14cff19f0f00cc
Marilin Tukia <marilin.tukia@websystems.ee>
2020-04-22 19:25:24 +0300

    first commit

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        
+        public string Category { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ Models/MovieCategoryModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+    public class MovieCategoryModel
+    {
+        public List<Movie> Movies { get; set; }
+
+        public SelectList Categories { get; set; }
+
+        public String MovieCategory { get; set; }
+
+        public String SearchString { get; set; }
+
+    }
+}
